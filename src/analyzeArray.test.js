3d01eb4d5fc83a6cd1eb99b7b02b13ee7093f35b
Letam Bossman Barinua <letambarinua@gmail.com>
2022-09-27 11:21:30 +0100

    Change toMatchObject to toEqual in analyzeArray.test.js

--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -2,7 +2,7 @@
 const analyzeArray = require("./analyzeArray");
 
 test("function that takes array return an object of average, min, max and length", () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toMatchObject({
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
     max: 8,
